f12700144da4c0dfe8b26350138b1916b55c6d87
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-08-01 22:05:20 -0300

    test: cria primeiros testes de sucesso e falha com o jest

--- src/controllers/tests/EmailController.spec.js
+++ src/controllers/tests/EmailController.spec.js
@@ -1,3 +1,27 @@
 // DESCRIBE --> declara um bloco de testes, reunindo eles (testsuites)
 // IT OR TEST --> declara um único teste unitário (testcases)
 // EXPECT --> faz asserções acerca do resultado do teste unitário 
+
+function sum(a, b) {
+    return a + b;
+}
+
+describe("Initial Tests", () => {
+    it("First unit tests", () => {
+        const firstArgument = 7;
+        const secondArgument = 1;
+
+        let result = sum(firstArgument, secondArgument);
+
+        expect(result).toEqual(firstArgument + secondArgument);
+    })
+
+    it("Second unit tests", () => {
+        const firstArgument = 7;
+        const secondArgument = 1;
+
+        let result = sum(firstArgument, secondArgument);
+
+        expect(result).toEqual(firstArgument + secondArgument + 1)
+    })
+})
