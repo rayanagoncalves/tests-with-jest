5c03c6a920550baa4d9f0eac48b46e7cf3a611cf
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-08-01 22:37:40 -0300

    test: cria testes de sucesso e erro do EmailController

--- src/controllers/tests/EmailController.spec.js
+++ src/controllers/tests/EmailController.spec.js
@@ -2,26 +2,67 @@
 // IT OR TEST --> declara um único teste unitário (testcases)
 // EXPECT --> faz asserções acerca do resultado do teste unitário 
 
-function sum(a, b) {
-    return a + b;
-}
+const EmailController = require('../EmailController');
+const EmailQueue = require('../../queue/MailQueue');
+const MailQueue = require('../../queue/MailQueue');
 
-describe("Initial Tests", () => {
-    it("First unit tests", () => {
-        const firstArgument = 7;
-        const secondArgument = 1;
+jest.mock('../../queue/MailQueue');
 
-        let result = sum(firstArgument, secondArgument);
+describe("Email Controller", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+    test("Should sent email successfully", async () => {
+        const request = {
+            body: {
+                email: "teste@example.com",
+                firstName: "John",
+                lastName: "Hill"
+            }
+        }
 
-        expect(result).toEqual(firstArgument + secondArgument);
+        const reply = {
+            code: jest.fn().mockReturnThis(),
+            send: jest.fn()
+        }
+
+        const template = `
+        Olá ${request.body.firstName} ${request.body.lastName}, sua assinatura foi confirmada!
+        Para acessar seus recursos exclusivos você precisa basta clicar aqui.
+    `
+
+        await EmailController.sendEmail(request, reply);
+
+        expect(EmailQueue.add).toHaveBeenCalledTimes(1);
+        expect(EmailQueue.add).toHaveBeenCalledWith({
+            to: "teste@example.com",
+            from: process.env.EMAIL_FROM,
+            subject: "Assinatura Confirmada",
+            text: template
+        });
+        expect(reply.code).toHaveBeenCalledWith(200);
     })
 
-    it("Second unit tests", () => {
-        const firstArgument = 7;
-        const secondArgument = 1;
+    test("Should return error when not send email", async () => {
+        const request = {
+            body: {
+                email: "teste@example.com",
+                firstName: "John",
+                lastName: "Hill"
+            }
+        }
 
-        let result = sum(firstArgument, secondArgument);
+        const reply = {
+            code: jest.fn().mockReturnThis(),
+            send: jest.fn()
+        }
 
-        expect(result).toEqual(firstArgument + secondArgument + 1)
+        MailQueue.add.mockRejectedValue(new Error("Mocking exception"));
+
+        await EmailController.sendEmail(request, reply);
+
+        expect(EmailQueue.add).toHaveBeenCalledTimes(1);
+        expect(reply.code).toHaveBeenCalledWith(500);
+        expect(reply.send).toHaveBeenCalledWith("Internal Server Error");
     })
 })
