89ca31b6455b90a545b068d0b75b33b4c3cf9105
rayanagoncalves <rayana.goncalves.cunha@hotmail.com>
2024-08-01 21:58:07 -0300

    feat: add dependencia do jest

---
+++ src/controllers/tests/EmailController.spec.js
@@ -0,0 +1,3 @@
+// DESCRIBE --> declara um bloco de testes, reunindo eles (testsuites)
+// IT OR TEST --> declara um único teste unitário (testcases)
+// EXPECT --> faz asserções acerca do resultado do teste unitário 
